5f5e660e7d5b7b4b1c170fa56643f6844ef8fe42
GianmarcoOrabona <gianmarcoorabona091@gmail.com>
2023-12-05 14:17:04 +0100

    add constructor

--- src/org/lessons/java/geometria/Main.java
+++ src/org/lessons/java/geometria/Main.java
@@ -1,2 +1,10 @@
-package org.lessons.java.geometria;public class Main {
+package org.lessons.java.geometria;
+
+public class Main {
+    public static void main(String[] args) {
+        Rettangolo rettangolo = new Rettangolo(20, 10);
+
+        System.out.println("Base: " + rettangolo.base);
+        System.out.println("Altezza: " + rettangolo.altezza);
+    }
 }
--- src/org/lessons/java/geometria/Rettangolo.java
+++ src/org/lessons/java/geometria/Rettangolo.java
@@ -1,2 +1,19 @@
-package org.lessons.java.geometria;public class Rettangolo {
+package org.lessons.java.geometria;
+
+public class Rettangolo {
+
+    // ATTRIBUTI
+    int base;
+    int altezza;
+
+    // COSTRUTTORI
+
+    // Costruttore che permette di inserire come parametri la base e l'altezza
+    public Rettangolo(int base, int altezza) {
+        this.base = base;
+        this.altezza = altezza;
+    }
+
+
+    // METODI
 }
